ae84059d52980faacd53fef371bde6501246c0a3
Yoshua Elmaryono <yoshuaelmaryono@10-60-48-89.0200.bsd.id.iosda.org>
2018-07-11 12:41:12 +0700

    finish bubbles generation

---
+++ transform animation/Bubble.swift
@@ -0,0 +1,22 @@
+//
+//  Bubble.swift
+//  transform animation
+//
+//  Created by Yoshua Elmaryono on 11/07/18.
+//  Copyright © 2018 Yoshua Elmaryono. All rights reserved.
+//
+
+import UIKit
+
+class Bubble: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+
+}
--- transform animation/ViewController.swift
+++ transform animation/ViewController.swift
@@ -14,12 +14,53 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var timer: Timer?
+    
+    @IBAction func screenTapped(_ sender: UITapGestureRecognizer) {
+        if timer == nil {
+            generateBubbles()
+        }else{
+            timer!.invalidate()
+            timer = nil
+        }
     }
+    
+    func getRandomColor() -> UIColor {
+        let red = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
+        let green = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
+        let blue = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+    
+    func generateBubbles(){
+        timer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(makeOneBubble), userInfo: nil, repeats: true)
+    }
+    @objc func makeOneBubble(){
+        let diameter = 20
+        let bottomScreenPosition = view.frame.maxY + CGFloat(diameter)
+        let x = arc4random_uniform(UInt32(view.frame.maxX - CGFloat(diameter)))
 
-
+        let bubbleFrame = CGRect(x: Int(x), y: Int(bottomScreenPosition), width: diameter, height: diameter)
+        var bubbleView: UIView? = UIView(frame: bubbleFrame)
+        bubbleView?.layer.cornerRadius = CGFloat(diameter)/2
+        bubbleView?.backgroundColor = getRandomColor()
+        view.addSubview(bubbleView!)
+        
+        let randomDuration = arc4random_uniform(10)
+        UIView.animate(
+            withDuration: TimeInterval(randomDuration),
+            delay: 0,
+            options: .curveLinear,
+            animations: {
+                bubbleView?.frame = CGRect(x: Int(x), y: -20, width: diameter, height: diameter)
+            },
+            completion: { finished in
+                bubbleView?.removeFromSuperview()
+                bubbleView = nil
+            }
+        )
+    }
 }
 
